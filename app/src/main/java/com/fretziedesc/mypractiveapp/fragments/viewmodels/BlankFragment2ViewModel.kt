22bc11d0aec61e45f6bb9e7c26e5aff95a9eac7f
fretziedesc <afmanigos@gmail.com>
2021-07-14 21:12:42 +0800

    remove unnecessary files

--- app/src/main/java/com/fretziedesc/mypractiveapp/fragments/viewmodels/BlankFragment2ViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.fretziedesc.mypractiveapp.fragments.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class BlankFragment2ViewModel : ViewModel() {
-  // TODO: Implement the ViewModel
-}
